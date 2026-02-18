932b812fa7dc583b6229d540acffb32f8c7567fa
ST10482816 <ST10482816@vcconnect.edu.za>
2026-02-18 21:20:17 +0200

    Cleaned Code

--- src/first_java_application/First_Java_Application.java
+++ src/first_java_application/First_Java_Application.java
@@ -1,20 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package first_java_application;
 
-/**
- *
- * @author lukeg
- */
+
 public class First_Java_Application {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello World");
     }
     
 }
